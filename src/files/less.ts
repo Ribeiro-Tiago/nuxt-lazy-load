1a96b904b340bf0a1f3741bf35f1607a738c0079
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2025-03-23 16:45:04 +0000

    fix: less import

--- src/files/less.ts
+++ src/files/less.ts
@@ -1,6 +1,6 @@
-import { render } from "less";
+import less from "less";
 import { readFileSync } from "node:fs";
 
 export const lessProcessor = async (filePath: string) => {
-  return (await render(readFileSync(filePath).toString(), { compress: true })).css;
+  return (await less.render(readFileSync(filePath).toString(), { compress: true })).css;
 };
